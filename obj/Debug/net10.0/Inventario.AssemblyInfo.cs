d2f93b695b072ba507ca9bc74aa3d21e00ded94b
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2025-12-18 17:21:56 -0500

    Usuario controller creado

--- obj/Debug/net10.0/Inventario.AssemblyInfo.cs
+++ obj/Debug/net10.0/Inventario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f5991f21e9ae3f4c408672f585b55a2d45ad95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5302a42b1a5ab3067a42daf8a727ab666f73d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

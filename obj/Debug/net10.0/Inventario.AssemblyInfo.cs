7c4c4cb85f45475fb1ae33ac116b8da620cfcd87
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2025-12-30 16:47:52 -0500

    Credenciales de azure creadas en el repositorio y en el pipeline

--- obj/Debug/net10.0/Inventario.AssemblyInfo.cs
+++ obj/Debug/net10.0/Inventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb27c5c14898c0988bdc741ffed1eb94089a5045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3235215fd92a9a1cc419ee6ba4e7270b652d9c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
